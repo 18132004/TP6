6bb8c2ccd9049f2251e28b1be9c7f5ded61eae25
Amaury <amaury.gaborit17@gmail.com>
2025-01-13 14:41:26 +0100

    a

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -1,16 +1,25 @@
 #include "Sudoku.h"
+#include <vector>
 
-Sudoku::Sudoku() {}
+Sudoku::Sudoku() {
+}
 
-void Sudoku::permuteCase(int pos1, int pos2) {
-    int temp = grid[pos1];
-    grid[pos1] = grid[pos2];
-    grid[pos2] = temp;
+std::array<std::vector<int>, 3> Sudoku::getNum(int pos1, int pos2) {
+    std::array<std::vector<int>, 3> num;
+    for (int i = 0; i < N; i++) {
+        num[0].push_back(i);
+        num[1].push_back(i);
+        num[2].push_back(i);
+    }
+
+    return num;
 }
 
 void Sudoku::generateGrid() {
-    for (int i = 0; i < N; i++) {
-        grid[i] = i % N;
+    for (int y = 0; y < N; y++) {
+        for (int x = 0; x < N; x++) {
+            std::vector<int> row;
+        }
     }
 
 }
